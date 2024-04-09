57c5210d72046c9d5c32765e2c2648a4cd10c399
Jimintae <jiax2@naver.com>
2024-04-09 16:37:12 +0900

    6주차 끝!

---
+++ chp7/EvCar.java
@@ -0,0 +1,26 @@
+
+public class EvCar extends Car {
+	int charge = 100;
+	
+	EvCar() {
+	super(2024, "Tesla", "Model X");
+	}
+	
+	int getCharge() {
+		return this.charge;
+	}
+	void charge() {
+		this.charge = 100;
+		System.out.println("You charged your car.");
+	}
+	
+	// Car함수 어버라이딩
+	void drive() {
+		this.charge -= 10;
+		this.km += 100;
+		
+		System.out.println("You drove "+
+				this.km + "km, and have " +
+				this.charge + "% battery.");
+	}
+}
